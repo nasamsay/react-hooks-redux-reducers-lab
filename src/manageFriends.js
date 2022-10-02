b4688f5964b3fd3e69a88dfd661e832ba4505246
Nasam Sayour <nasamsay@gmail.com>
2022-10-02 14:33:44 +0300

    Complete assignment

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,15 @@
 export function manageFriends(state, action) {
-  // your code here
+  const newState = {...state}
+  if (action.type === "friends/add") {
+    newState.friends.push(action.payload)
+    return newState
+  }
+  else if (action.type === "friends/remove") {
+    newState.friends = newState.friends.filter(friend => friend.id != action.payload)
+    return newState
+
+   }
+   else {
+    return newState
+   }
 }
