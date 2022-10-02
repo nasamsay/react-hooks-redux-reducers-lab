3629fad5bd5d41a752e820024948733d61ba1e38
Nasam Sayour <nasamsay@gmail.com>
2022-10-02 14:33:28 +0300

    commit tree 2c9bc96ac51b149b58135ebaa386ddfe53ca34b8

--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,16 @@
 const initialState = { numberOfPresents: 0 };
 
+const action = {
+  type: "presents/increase",
+}
+
 export function managePresents(state = initialState, action) {
-  // your code here
+  const newState = {numberOfPresents : state.numberOfPresents}
+  if (action.type === "presents/increase") {
+    newState.numberOfPresents += 1
+    return newState
+  }
+  else {
+    return newState
+  }
 }
